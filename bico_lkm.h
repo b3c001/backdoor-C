e4b44c93ab8507ffdae77b2cf4ee5b5d9fc6ae94
becool <marcelopizzol@proton.me>
2025-04-30 15:06:30 -0300

    Create bico_lkm.h

---
+++ bico_lkm.h
@@ -0,0 +1,8 @@
+#ifndef BICO_LKM_H
+#define BICO_LKM_H
+
+#define HIDE_PORT 4444      // ocultar porta
+#define ROOT_SIGNAL 44      // Signal to root
+#define MAGIC_PREFIX "bico" // Prefixo ocultar PID 
+
+#endif
